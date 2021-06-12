fcc75e8e0e1ac500e4d445b03d64f71f74fb7036
Hai Bui <80174261+Hasumeri@users.noreply.github.com>
2021-06-12 15:00:21 +0700

    seperate user, cook and manager website

--- server/middleware/author.js
+++ server/middleware/author.js
@@ -1,6 +1,6 @@
 const jsonwebtoken = require('jsonwebtoken')
 
-const verifyToken = (req, res, next) => {
+const verifyTokenUser = (req, res, next) => {
     const authHeader = req.header('Authorization')
     const token = authHeader && authHeader.split(' ')[1]
 
@@ -18,4 +18,40 @@ const verifyToken = (req, res, next) => {
     }
 }
 
-module.exports = verifyToken
+const verifyTokenCook = (req, res, next) => {
+    const authHeader = req.header('Authorization')
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token)
+        return res.json({success: false, message: 'Access token not found'})
+    
+    try {
+        const decodedData = jsonwebtoken.verify(token, process.env.ACCESS_TOKEN)
+        req.cookId = decodedData.cookId
+        next()
+    }
+    catch (error) {
+        console.log(error)
+        return res.json({success: false, message: 'Invalid token'})
+    }
+}
+
+const verifyTokenManager = (req, res, next) => {
+    const authHeader = req.header('Authorization')
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token)
+        return res.json({success: false, message: 'Access token not found'})
+    
+    try {
+        const decodedData = jsonwebtoken.verify(token, process.env.ACCESS_TOKEN)
+        req.managerId = decodedData.managerId
+        next()
+    }
+    catch (error) {
+        console.log(error)
+        return res.json({success: false, message: 'Invalid token'})
+    }
+}
+
+module.exports = {verifyTokenUser, verifyTokenCook, verifyTokenManager}
