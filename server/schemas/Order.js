7dbec6e5b99c7dbe9b8db67bcdad9aaa0313bde8
Hai Bui <80174261+Hasumeri@users.noreply.github.com>
2021-06-13 22:25:45 +0700

    update Schema

--- server/schemas/Order.js
+++ server/schemas/Order.js
@@ -2,16 +2,47 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const OrderSchema = new Schema({
+    listFood: [{
+        foodName: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        quantity: {
+            type: Number,
+            required:true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        amount: {
+            type: Number,
+            required: true
+        }
+    }],
+
+    total: {
+        type: Number,
+        required: true
+    },
+
     status: {
         type: String,
         enum: ['waiting', 'pending', 'processing', 'completed'],
-        require: true
+        required: true
+    },
+
+    createAt: {
+        type: Date,
+        default: Date.now
     },
 
     user: {
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
+
 })
 
 module.exports = mongoose.model('Order', OrderSchema)
