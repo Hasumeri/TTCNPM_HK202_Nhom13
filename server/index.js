e5870b2e666e00f5937ada4fe3f9f5de00f9db6a
Hai Bui <80174261+Hasumeri@users.noreply.github.com>
2021-06-19 17:03:03 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const food = require('./api/food')
 const foodHandler = require("./api/foodHandler");
 const orderHandler = require("./api/orderHandler")
 const searchHandler = require("./api/searchHandler");
+const addFoodHandler = require('./api/addNewFoodHandler')
+const payment = require('./api/testing_pushOrderHandler')
 require('dotenv').config()
 
 connectDatabase()
@@ -21,7 +23,8 @@ server.use('/', food)
 server.use('/api/authen', authen)
 server.use('/api/foodHandler', foodHandler)
 server.use('/api/orderHandler', orderHandler)
-
+server.use('/api/addFood', addFoodHandler)
+server.use("/orders", payment);
 
 const PORT = 5000
 
