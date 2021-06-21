a4bd18b4f0a0265dba28fe167cd48c94bd3fab87
tranducmanh235 <tranducmanh235@gmail.com>
2021-06-21 20:07:46 +0700

    xoa AddFood.js

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch,Link} from 'react-router-dom'
 import Home from './components/views/Home'
 import Signin from './components/views/Signin'
-import AddFood from './components/views/AddFood'
 import ModifyFood from './components/views/ModifyFood'
 import DeleteFood from './components/views/DeleteFood'
 import ManagerContextProvider from './components/contexts/ManagerContext'
@@ -19,16 +18,9 @@ function App() {
             <Switch>
               <Route exact path='/' component={Home} />
               <Route exact path='/signin' component={Signin} />
-              <Route exact path='/addfood' component={AddFood} />
               <Route exact path='/modifyfood' component={ModifyFood} />
               <Route exact path='/deletefood' component={DeleteFood} />
 
-              {/* <Route exact path='/deletefood'>
-                <DeleteFood/>
-              </Route>
-              <Route exact path='/deletefood/2'>
-                <DeleteFood2/>
-              </Route> */}
             </Switch>
           
         </FoodContextProvider>
